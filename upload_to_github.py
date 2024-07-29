e1082439b42b2fb7b7b88a2e6c6b6d9cde6d2d6c
metinciris <metin@metinciris.com.tr>
2024-07-29 18:37:45 +0300

    Update upload_to_github.py

--- upload_to_github.py
+++ upload_to_github.py
@@ -10,7 +10,8 @@ os.environ['GIT_PYTHON_GIT_EXECUTABLE'] = r'C:\Program Files\Git\bin\git.exe'
 # Ayarlar
 local_repo_path = 'C:/Users/user/Documents/ses'  # Yerel repo yolu
 remote_repo_url = 'https://github.com/metinciris/patolojises.git'  # Uzak repo URL'si
-mc_folder = os.path.join(local_repo_path, 'project', 'mc')  # Yüklenecek mc klasörü
+audio_files_folder = os.path.join(local_repo_path, 'project/mc/audio_files')  # Audio files folder
+transcriptions_folder = os.path.join(local_repo_path, 'project/mc/transcriptions')  # Transcriptions folder
 
 # GitHub deposunu mevcut yerel depo üzerinden kullan
 repo = git.Repo(local_repo_path)
@@ -32,26 +33,24 @@ def get_remote_files():
         print(f"Failed to get remote files: {e}")
     return remote_files
 
-def upload_files():
-    remote_files = get_remote_files()
+def upload_files(folder_path, target_folder):
     current_files = set()
-    for subdir, dirs, files in os.walk(mc_folder):
+    for subdir, dirs, files in os.walk(folder_path):
         for file in files:
-            relative_path = os.path.relpath(os.path.join(subdir, file), mc_folder)
-            if relative_path not in remote_files:
-                current_files.add(os.path.join('mc', relative_path))  # mc klasörüne göre yolu ayarla
+            relative_path = os.path.relpath(os.path.join(subdir, file), local_repo_path)
+            current_files.add(relative_path)
 
     if current_files:
         print(f"Yeni dosyalar bulundu: {current_files}")
-        repo.index.add([os.path.join('project', 'mc', file) for file in current_files])
-        repo.index.commit(f"Yeni dosyalar eklendi: {list(current_files)}")
-        
+        repo.git.add(all=True)
+        repo.index.commit(f"Yeni dosyalar eklendi: {current_files}")
+
         # Uzak depodan güncellemeleri çek ve birleştir
         try:
             repo.git.pull('origin', 'main', '--rebase')
         except Exception as e:
             print(f"Failed to pull remote changes: {e}")
-        
+
         # Değişiklikleri uzak depoya gönder
         try:
             repo.git.push("origin", "main")
@@ -62,7 +61,8 @@ def upload_files():
 def start_upload():
     root.withdraw()
     while True:
-        upload_files()
+        upload_files(audio_files_folder, 'mc/audio_files')
+        upload_files(transcriptions_folder, 'mc/transcriptions')
         time.sleep(10)  # 10 saniyede bir kontrol et
 
 root = tk.Tk()
